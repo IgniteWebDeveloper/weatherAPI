d0215dfa432b5950f4ac3006ee3077afef9075e3
Syed Adnan Ali <61422258+IgniteWebDeveloper@users.noreply.github.com>
2022-02-01 00:19:21 +0530

    Frontend Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-;
+import Cities from './cities.json';
+
 
 
 const App = () => {
   return <div className='container'>
     <Typography>
       <h2><br /> Welcome to weather app <br />Enter the city name to see its weather</h2>
-
-      <div className="innerContainer">
-        <input type="text" name="" id="" />
-
-      </div>
     </Typography>
+      <div className="innerContainer">
+        <input className='searchBar' type="text" name="" id="" />
+        <div className='searchDrop'>
+          <p>Lorem</p>
+        </div>
+      </div>
   </div>;
 };
 
