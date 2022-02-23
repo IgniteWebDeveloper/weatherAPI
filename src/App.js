327fe7e2d9d4b7bb5ddf3dcfa8feb86c21d3c877
Syed Adnan Ali <61422258+IgniteWebDeveloper@users.noreply.github.com>
2022-02-23 20:43:09 +0530

    New Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Typography, Card, Box, CardContent, CardMedia } from '@mui/material';
+import { Typography, Card, Box, CardContent, CardMedia, Grid } from '@mui/material';
 import Cities from './cities.json';
 import { Search, Close } from '@mui/icons-material';
 import axios from 'axios';
@@ -14,7 +14,7 @@ import { useTheme } from '@mui/material/styles';
 
 const App = () => {
 
-  const [searchWord, setSearchWord] = useState("")
+  const [searchWord, setSearchWord] = useState([])
   const [filterSearch, setFilter] = useState({})
   const [weatherData, setweatherData] = useState([])
 
@@ -33,7 +33,11 @@ const App = () => {
     axios.get(base_url)
       .then(function (response) {
         // handle success
-        setweatherData(response.data)
+        localStorage.setItem("city", JSON.stringify(response.data))
+        const copycityWeather = [...weatherData]
+        const newcityData = response.data
+        copycityWeather.push(newcityData)
+        setweatherData(copycityWeather)
       })
       .catch(function (error) {
         // handle error
@@ -45,15 +49,15 @@ const App = () => {
   const allClear = () => {
     setFilter([])
     setSearchWord("")
-    weatherData([])
   }
   const theme = useTheme();
 
-  console.log(weatherData);
+  console.log(weatherData.map(city => city));
 
   return <div className='container'>
-      <h2><br /> Welcome to Weather App <br />Enter the city name to see its weather</h2>
-    <input className='searchBar' type="text" value={searchWord} onChange={(event) => searchingWord(event)} name="" />{searchWord.length == 0 ?
+    <div className="mainContainer1">
+    <h2><br /> Welcome to Weather App <br />Enter the city name to see its weather</h2>
+    <input className='searchBar' type="text" value={searchWord} onChange={(event) => searchingWord(event)} name="" />{searchWord.length === 0 ?
       <Search className='Icon' /> : <Close onClick={allClear} className='CloseIcon' />}
     {searchWord.length && filterSearch.length !== 0 ?
       <div className='searchDrop'>
@@ -62,29 +66,54 @@ const App = () => {
         }).slice(0, 15)}
       </div>
       : ''}
-    {weatherData.length !==0 ? <div className='card'>
-    <Card sx={{ display: 'flex', width: '22vmax' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5" mt={2}>
-            {weatherData.location.name}
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            <b>{weatherData.current.temp_c}°C</b>
-            <p className='time'>{weatherData.current.last_updated}</p>
-          </Typography>
-        </CardContent>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image={weatherData.current.condition.icon}
-        alt="Live from space album cover"
-      />
-    </Card>
-    </div> : ''}
-    
+    </div>
+      <Grid className="mainContainer2" container spacing={{ xs: 2, md: 1 }} columns={{ xs: 4, sm: 12, md: 12 }}>
+        {weatherData.map((cityData, index) =>
+          <Grid item xs={2} sm={4} md={4}  key={index} style={{width: '23vmax', height: '20vmax', marginBottom: '0vmax'}} >
+            <Card sx={{ display: 'flex', width: '22vmax' }}>
+                <CardContent>
+                  <Typography component="div" variant="h5">
+                    {cityData.location.name}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary" component="div">
+                    <b>{cityData.current.temp_c}°C</b>
+                    <p className='time'>{cityData.current.last_updated}</p>
+                  </Typography>
+                </CardContent>
+              <CardMedia
+                component="img"
+                sx={{ width: 100, display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                image={cityData.current.condition.icon}
+                alt="Live from space album cover"
+              />
+            </Card>
+          </Grid>
+        )}
+      </Grid>
+
   </div>
 };
 
 export default App;
+
+
+
+{/* <Card sx={{ display: 'flex', width: '22vmax' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography component="div" variant="h5" mt={2}>
+              {cityData.location.name}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              <b>{cityData.current.temp_c}°C</b>
+              <p className='time'>{cityData.current.last_updated}</p>
+            </Typography>
+          </CardContent>
+        </Box>
+        <CardMedia
+          component="img"
+          sx={{ width: 151 }}
+          image={cityData.current.condition.icon}
+          alt="Live from space album cover"
+        />
+      </Card> */}
